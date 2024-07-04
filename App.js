30bc96d609f987b3a47a137f24375d0c64a8695e
shakti prasad sahu <shaktisahu72@gmail.com>
2024-07-04 22:11:03 +0530

    episod-1

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id: "heading", xyz : "1000"}, "Hello Namaste React !!")
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
